452a2c2818cb5f0427a91e7ca63b0a19b74c2ee1
Paulo Valente Maia <paulovalentemaia@gmail.com>
2021-06-25 07:55:15 -0300

    Criado Button Login Social - Add VoidCallback onTap e InkWell

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -56,7 +56,11 @@ class _LoginPageState extends State<LoginPage> {
                     Padding(
                       padding:
                           const EdgeInsets.only(top: 40, left: 40, right: 40),
-                      child: SocialLoginButton(),
+                      child: SocialLoginButton(
+                        onTap: () {
+                          print("clicou");
+                        },
+                      ),
                     )
                   ],
                 ),
