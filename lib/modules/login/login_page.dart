15d3fe06deb4926979d79290242793ea46eb9718
Paulo Valente Maia <paulovalentemaia@gmail.com>
2021-06-25 15:53:04 -0300

    Social login alterado para pegar apenas email

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -57,8 +58,18 @@ class _LoginPageState extends State<LoginPage> {
                       padding:
                           const EdgeInsets.only(top: 40, left: 40, right: 40),
                       child: SocialLoginButton(
-                        onTap: () {
-                          print("clicou");
+                        onTap: () async {
+                          GoogleSignIn _googleSignIn = GoogleSignIn(
+                            scopes: [
+                              'email',
+                            ],
+                          );
+                          try {
+                            final response = await _googleSignIn.signIn();
+                            print(response);
+                          } catch (error) {
+                            print(error);
+                          }
                         },
                       ),
                     )
