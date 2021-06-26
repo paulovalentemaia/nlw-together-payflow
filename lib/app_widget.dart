e9f481f9c38eeef584ed5d288557907c4b82b25a
paulo valente maia <paulovalentemaia@gmail.com>
2021-06-26 17:28:02 -0300

    Adicionado rota e definido splash rota inicial

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,16 +1,22 @@
 import 'package:flutter/material.dart';
+import 'modules/home/home_page.dart';
 import 'modules/login/login_page.dart';
+import 'modules/splash/splash_page.dart';
 import 'shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'PayFLow',
-      theme: ThemeData(
-        primaryColor: AppColors.primary,
-      ),
-      home: LoginPage(),
-    );
+        title: 'PayFLow',
+        theme: ThemeData(
+          primaryColor: AppColors.primary,
+        ),
+        initialRoute: "/splash",
+        routes: {
+          "/splash": (context) => SplashPage(),
+          "/home": (context) => HomePage(),
+          "/login": (context) => LoginPage()
+        });
   }
 }
